c9cfd49fa15aa172c27eb047729d426bbfb39d2c
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-09-03 18:48:33 -0400

    vuex token saved in vuex

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
+import './plugins/bootstrap-vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
+
   router,
   store,
+  methods: {
+    // Nuestra función
+    init () {
+      store.dispatch('oauth/getToken', null, { root: true })
+    }
+  },
+  // Hook created
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
